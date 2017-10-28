67f73dfef2a2acb5fa7f60f53971d86d2ac17778
bkustar <bkustar@gmail.com>
2017-10-28 15:30:13 -0500

    portfolio models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Portfolio(models.Model):
+	totalInvestment = models.DecimalField(max_digits = 15, decimal_places = 2)
+
+class Position(models.Model):
+	portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+	assetAmount = models.DecimalField(max_digits = 20, decimal_places = 8)
+	portfolioPercentage = models.DecimalField(max_digits = 11, decimal_places = 8)
