2cf3e40cca86c5a2d80ed3ff4933f386b697bac7
Sean Geiger <sean@geigers.org>
2017-10-28 15:42:05 -0500

    user models

--- hivemind/settings.py
+++ hivemind/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio.apps.PortfolioConfig',
+    'users.apps.UsersConfig',
+    'assets.apps.AssetsConfig'
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_investments = models.DecimalField(max_digits=15, decimal_places=2)
+    original_investment = models.DecimalField(max_digits=15, decimal_places=2)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Preference(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    BULL = 'BULL'
+    BEAR = 'BEAR'
+    BEE = 'BEE'
+    PREF_CHOICES = (
+        (BULL, 'Bull'),
+        (BEAR, 'Bear'),
+        (BEE, 'Bee'),
+    )
+    preference = models.CharField(max_length=5, choices=PREF_CHOICES, blank=False, default=BEE)
